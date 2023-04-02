ee3206031a58fe8577ef16c0a17cbb21ad07fe26
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-04-02 12:55:30 +0200

    adding cors

--- server.js
+++ server.js
@@ -17,6 +17,16 @@ connectDB();
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
+
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
