16d2321bcd1caf9d867af749ef76694551fc3188
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-04-07 12:26:56 +0200

    adding call for request to pay

--- controllers/eswavasController.js
+++ controllers/eswavasController.js
@@ -12,12 +12,41 @@ const collectionsRequestToPay = asyncHandler(async (req, res) => {
       password: process.env.MOMO_PASSWORD,
     }),
   };
+
   try {
-    const accessToken = await fetch(
+    const accessTokenResponse = await fetch(
       "https://eswavas-api.herokuapp.com/auth/",
       config
     );
-    console.log(await accessToken.json());
+    const accessToken = await accessTokenResponse.json();
+
+    // Request to pay
+    const requestToPayConfig = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken.access_token}`,
+      },
+      body: JSON.stringify({
+        amount: "50.0",
+        currency: "EUR",
+        externalId: "6353636",
+        payer: {
+          partyIdType: "MSISDN",
+          partyId: "0248888736",
+        },
+        payerMessage: "Second request to pay",
+        payeeNote: "payer note",
+      }),
+    };
+
+    const requestToPay = await fetch(
+      "https://eswavas-api.herokuapp.com/collection/request-to-pay",
+      requestToPayConfig
+    );
+
+    res.status(202).json(await requestToPay.json());
   } catch (error) {}
 
   const createdOrder = await order.save();
