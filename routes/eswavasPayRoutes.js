0da37d04ac907c8cdbce15739c201721f8f16cd5
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-04-13 13:10:00 +0200

    adding get account status route

--- routes/eswavasPayRoutes.js
+++ routes/eswavasPayRoutes.js
@@ -3,6 +3,7 @@ import { protect } from "../middleware/authMiddleware.js";
 import {
   collectionsRequestToPay,
   collectionsGetAccountBalance,
+  collectionsGetAccountStatus,
 } from "../controllers/eswavasController.js";
 
 const router = express.Router();
@@ -14,5 +15,9 @@ router
 router
   .route("/collections/getAccountBalance")
   .get(protect, collectionsGetAccountBalance);
+
+router
+  .route("/collections/getMomoAccountStatus")
+  .get(protect, collectionsGetAccountStatus);
 
 export default router;
